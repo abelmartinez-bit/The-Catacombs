1abf00fa3a66aaec4cc2e19ec2d3ca4861f1386b
oamu2007 <oamu2007@gmail.com>
2026-04-16 21:02:45 -0700

    Añadi los json del SFML para VS Code

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,20 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
 
-int main() {
-    std::cout << "Test" << std::endl;
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode({800, 600}), "The Catacombs");
+
+    while (window.isOpen())
+    {
+        while (const std::optional event = window.pollEvent())
+        {
+            if (event->is<sf::Event::Closed>())
+                window.close();
+        }
+
+        window.clear(sf::Color::Black);
+        window.display();
+    }
+
     return 0;
 }
